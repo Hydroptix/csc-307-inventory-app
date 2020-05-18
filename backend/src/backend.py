b9432b9d9146b69e64380721ff7b24d13032cd15
Tyler <tyler_england35@yahoo.com>
2020-05-18 10:54:45 -0700

    Creating Endpoints in backend

--- backend/src/backend.py
+++ backend/src/backend.py
@@ -1,6 +1,79 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
 app = Flask(__name__)
+
+
+users = { 
+   'users_list' :
+   [
+      { 
+         'id' : 'xyz789',
+         'name' : 'Charlie',
+         'job': 'Janitor',
+      },
+      {
+         'id' : 'abc123', 
+         'name': 'Mac',
+         'job': 'Bouncer',
+      },
+      {
+         'id' : 'ppp222', 
+         'name': 'Mac',
+         'job': 'Professor',
+      }, 
+      {
+         'id' : 'yat999', 
+         'name': 'Dee',
+         'job': 'Aspring actress',
+      },
+      {
+         'id' : 'zap555', 
+         'name': 'Dennis',
+         'job': 'Bartender',
+      }
+   ]
+}
+
+
+@app.route('/users', methods=['GET', 'POST'])
+def get_users():
+   if request.method == 'GET':
+      search_username = request.args.get('name')
+      search_userjob = request.args.get('job')
+      if search_username:
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['name'] == search_username and user['job'] == search_userjob:
+               subdict['users_list'].append(user)
+         return subdict
+      return users
+   elif request.method == 'POST':
+      userToAdd = request.get_json()
+      users['users_list'].append(userToAdd)
+      resp = jsonify(success=True)
+      return resp
+
+
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
+def get_user(id):
+   if request.method == 'GET':
+      if id :
+         for user in users['users_list']:
+           if user['id'] == id:
+              return user
+         return ({})
+      return users
+   elif request.method == 'DELETE':
+      if id :
+         for i in range(len(users['users_list'])):
+           if users['users_list'][i]['id'] == id:
+              del users['users_list'][i]
+              return users
+      return ({})
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
