ccc684d5f5a334c3b83878b0fd09b909e3ec0a29
hydroptix <samuelfrazee@hotmail.com>
2020-05-20 12:38:55 -0700

    Move tests into src folder to make building simpler

--- backend/definitions.py
+++ backend/definitions.py
@@ -3,5 +3,4 @@ from os.path import dirname, abspath
 # Root backend directory
 ROOT_DIR = dirname(abspath(__file__))
 SRC_DIR = ROOT_DIR + "/src"
-TEST_DIR = ROOT_DIR + "/test"
 CONF_PATH = ROOT_DIR + "/conf"
