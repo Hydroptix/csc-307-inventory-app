b9b70ae817b7185773a4a27b230a066f78e32d0c
hydroptix <samuelfrazee@hotmail.com>
2020-06-06 14:50:05 -0700

    Small fix to login page render function actually make it return the html element

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react'
 class Login extends Component {
 
   render () {
-    <div className="container">
-      This is the login page
-    </div>
+    return (
+      <div className="container">
+        This is the login page
+      </div>
+    )
   }
 
 }
