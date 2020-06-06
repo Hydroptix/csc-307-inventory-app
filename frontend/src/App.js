4595f5beec652170618a54ac6c55eccf7801e1e4
hydroptix <samuelfrazee@hotmail.com>
2020-06-06 14:48:49 -0700

    Refactor app.js into AllSongs.js and create new App.js with page routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,69 +1,18 @@
-import React, { Component } from 'react'
-import Table from './Table'
-import Form from './Form'
-import axios from 'axios'
+import React, { Suspense, lazy } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+const Login = lazy(() => import('./Login'))
+const AllSongs = lazy(() => import('./AllSongs'))
 
-class App extends Component {
-   state = {
-      characters: []
-   }
-   
-   removeCharacter = index => {
-      const { characters } = this.state
-      
-      axios.delete("http://localhost:5000/users/".concat(characters[index].id))
-
-      this.setState({
-         characters: characters.filter((character, i) => {
-            return i !== index
-         }),
-      })
-   }
-
-   handleSubmit = character => {
-      this.makePostCall(character).then( callResult => {
-         if (callResult.status === 201) {
-            this.setState({ characters: [...this.state.characters, callResult.data] });
-         }
-      });
-   }
-
-
-   makePostCall(character){
-      return axios.post('http://localhost:5000/songs', character)
-       .then(function (response) {
-         console.log(response);
-         return response;
-       })
-       .catch(function (error) {
-         console.log(error);
-         return false;
-       });
-   }
-
-
-   render() {
-      const { characters } = this.state
-
-      return (
-        <div className="container">
-          <Table characterData={characters} removeCharacter={this.removeCharacter} />
-          <Form handleSubmit={this.handleSubmit} />
-        </div>
-      )
-   }
-
-   componentDidMount() {
-      axios.get('http://localhost:5000/songs').then(res => {
-          const characters = res.data.songs;
-          this.setState({ characters });
-      })
-      .catch(function (error) {
-          //Not handling the error. Just logging into the console.
-          console.log(error);
-      });
-  }
-}
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path="/" component={Login}/>
+        <Route path="/about" component={AllSongs}/>
+      </Switch>
+    </Suspense>
+  </Router>
+)
 
 export default App
