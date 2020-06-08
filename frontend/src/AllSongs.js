21dc66ceb74ee5e4451729b3d51e3494bb340f80
hydroptix <samuelfrazee@hotmail.com>
2020-06-07 19:11:05 -0700

    Refactor App.js to AllSongs.js

---
+++ frontend/src/AllSongs.js
@@ -0,0 +1,120 @@
+import React, { Component } from 'react'
+import Table from './Table'
+import Form from './Form'
+import axios from 'axios'
+
+class AllSongs extends Component {
+  state = {
+    songs: [],
+    playlists: []
+  }
+
+  removeSong = index => {
+    const { songs } = this.state
+
+    axios.delete('http://localhost:5000/songs/'.concat(songs[index].id))
+
+    this.setState({
+      songs: songs.filter((song, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+  removePlaylist = index => {
+    const { playlists } = this.state
+
+    axios.delete('http://localhost:5000/inventories/'.concat(playlists[index]._id))
+
+    this.setState({
+      playlists: playlists.filter((playlist, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+  handleSubmit = playlist => {
+    this.makePostCallPlaylist(playlist).then(callResult => {
+      if (callResult.status === 201) {
+        this.setState({ playlists: [...this.state.playlists, callResult.data] })
+      }
+    })
+  }
+
+  makePostCallPlaylist (playlist) {
+    return axios.post('http://localhost:5000/inventories', playlist)
+      .then(function (response) {
+        console.log(response)
+        return response
+      })
+      .catch(function (error) {
+        console.log(error)
+        return false
+      })
+  }
+
+  selectPlaylist = index => {
+    const { playlists } = this.state
+
+    console.log('Running select')
+
+    this.setState({
+      songs: []
+    })
+
+    //Need to preserve this so we can access the state from inside the map statement
+    let thisComponent = this
+
+    axios.get('http://localhost:5000/inventories/'.concat(playlists[index]._id)).then(res => {
+      const song_ids = res.data.songs
+
+      song_ids.map((id) => {
+        return axios.get('http://localhost:5000/songs/'.concat(String(id))).then(function (res) {
+          thisComponent.setState({
+            songs: [...thisComponent.state.songs, res.data]
+          })
+        })
+      })
+    })
+  }
+
+  render () {
+    const { songs } = this.state
+    const { playlists } = this.state
+
+    console.log(JSON.stringify(songs))
+
+    return (
+      <div className="container">
+        <Table songData={songs}
+               playlistData={playlists}
+               removeSong={this.removeSong}
+               removePlaylist={this.removePlaylist}
+               selectPlaylist={this.selectPlaylist}
+               addSong={this.addSong}/>
+
+        <Form handleSubmit={this.handleSubmit}/>
+      </div>
+    )
+  }
+
+  componentDidMount () {
+    axios.get('http://localhost:5000/songs').then(res => {
+      const songs = res.data.songs
+      this.setState({ songs })
+    })
+      .catch(function (error) {
+        //Not handling the error. Just logging into the console.
+        console.log(error)
+      })
+    axios.get('http://localhost:5000/inventories').then(res => {
+      const playlists = res.data.playlists
+      this.setState({ playlists })
+    })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }
+}
+
+export default AllSongs
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,117 +3,66 @@ import Table from './Table'
 import Form from './Form'
 import axios from 'axios'
 
+
 class App extends Component {
-  state = {
-    songs: [],
-    playlists: []
-  }
+   state = {
+      characters: []
+   }
+   
+   removeCharacter = index => {
+      const { characters } = this.state
+      
+      axios.delete("http://localhost:5000/users/".concat(characters[index].id))
 
-  removeSong = index => {
-    const { songs } = this.state
+      this.setState({
+         characters: characters.filter((character, i) => {
+            return i !== index
+         }),
+      })
+   }
 
-    axios.delete('http://localhost:5000/songs/'.concat(songs[index].id))
+   handleSubmit = character => {
+      this.makePostCall(character).then( callResult => {
+         if (callResult.status === 201) {
+            this.setState({ characters: [...this.state.characters, callResult.data] });
+         }
+      });
+   }
 
-    this.setState({
-      songs: songs.filter((song, i) => {
-        return i !== index
-      }),
-    })
-  }
 
-  removePlaylist = index => {
-    const { playlists } = this.state
+   makePostCall(character){
+      return axios.post('http://localhost:5000/songs', character)
+       .then(function (response) {
+         console.log(response);
+         return response;
+       })
+       .catch(function (error) {
+         console.log(error);
+         return false;
+       });
+   }
 
-    axios.delete('http://localhost:5000/inventories/'.concat(playlists[index]._id))
 
-    this.setState({
-      playlists: playlists.filter((playlist, i) => {
-        return i !== index
-      }),
-    })
-  }
+   render() {
+      const { characters } = this.state
 
-  handleSubmit = playlist => {
-    this.makePostCallPlaylist(playlist).then(callResult => {
-      if (callResult.status === 201) {
-        this.setState({ playlists: [...this.state.playlists, callResult.data] })
-      }
-    })
-  }
+      return (
+        <div className="container">
+          <Table characterData={characters} removeCharacter={this.removeCharacter} />
+          <Form handleSubmit={this.handleSubmit} />
+        </div>
+      )
+   }
 
-  makePostCallPlaylist (playlist) {
-    return axios.post('http://localhost:5000/inventories', playlist)
-      .then(function (response) {
-        console.log(response)
-        return response
+   componentDidMount() {
+      axios.get('http://localhost:5000/songs').then(res => {
+          const characters = res.data.songs;
+          this.setState({ characters });
       })
       .catch(function (error) {
-        console.log(error)
-        return false
-      })
-  }
-
-  selectPlaylist = index => {
-    const { playlists } = this.state
-
-    console.log('Running select')
-
-    this.setState({
-      songs: []
-    })
-
-    //Need to preserve this so we can access the state from inside the map statement
-    let thisComponent = this
-
-    axios.get('http://localhost:5000/inventories/'.concat(playlists[index]._id)).then(res => {
-      const song_ids = res.data.songs
-
-      song_ids.map((id) => {
-        return axios.get('http://localhost:5000/songs/'.concat(String(id))).then(function (res) {
-          thisComponent.setState({
-            songs: [...thisComponent.state.songs, res.data]
-          })
-        })
-      })
-    })
-  }
-
-  render () {
-    const { songs } = this.state
-    const { playlists } = this.state
-
-    console.log(JSON.stringify(songs))
-
-    return (
-      <div className="container">
-        <Table songData={songs}
-               playlistData={playlists}
-               removeSong={this.removeSong}
-               removePlaylist={this.removePlaylist}
-               selectPlaylist={this.selectPlaylist}
-               addSong={this.addSong}/>
-
-        <Form handleSubmit={this.handleSubmit}/>
-      </div>
-    )
-  }
-
-  componentDidMount () {
-    axios.get('http://localhost:5000/songs').then(res => {
-      const songs = res.data.songs
-      this.setState({ songs })
-    })
-      .catch(function (error) {
-        //Not handling the error. Just logging into the console.
-        console.log(error)
-      })
-    axios.get('http://localhost:5000/inventories').then(res => {
-      const playlists = res.data.playlists
-      this.setState({ playlists })
-    })
-      .catch(function (error) {
-        console.log(error)
-      })
+          //Not handling the error. Just logging into the console.
+          console.log(error);
+      });
   }
 }
 
