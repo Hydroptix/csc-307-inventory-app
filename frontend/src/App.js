48a4018071916a18047f6581516bbd7a5656f1f0
hydroptix <samuelfrazee@hotmail.com>
2020-06-07 18:53:26 -0700

    Get playlist selection to work by replacing a for loop with a map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,112 +3,117 @@ import Table from './Table'
 import Form from './Form'
 import axios from 'axios'
 
-
 class App extends Component {
-   state = {
-      songs: [],
-      playlists: []
-   }
-   
-   removeSong = index => {
-      const { songs } = this.state
-      
-      axios.delete("http://localhost:5000/songs/".concat(songs[index].id))
+  state = {
+    songs: [],
+    playlists: []
+  }
 
-      this.setState({
-         songs: songs.filter((song, i) => {
-            return i !== index
-         }),
-      })
-   }
+  removeSong = index => {
+    const { songs } = this.state
 
+    axios.delete('http://localhost:5000/songs/'.concat(songs[index].id))
 
-   removePlaylist = index => {
-      const { playlists } = this.state
+    this.setState({
+      songs: songs.filter((song, i) => {
+        return i !== index
+      }),
+    })
+  }
 
-      axios.delete("http://localhost:5000/inventories/".concat(playlists[index]._id))
+  removePlaylist = index => {
+    const { playlists } = this.state
 
-      this.setState({
-         playlists: playlists.filter((playlist, i) => {
-            return i !== index
-         }),
-      })
-   }
+    axios.delete('http://localhost:5000/inventories/'.concat(playlists[index]._id))
 
+    this.setState({
+      playlists: playlists.filter((playlist, i) => {
+        return i !== index
+      }),
+    })
+  }
 
-   handleSubmit = playlist => {
-      this.makePostCallPlaylist(playlist).then( callResult => {
-         if (callResult.status === 201) {
-            this.setState({ playlists: [...this.state.playlists, callResult.data] });
-         }
-      });
-   }
+  handleSubmit = playlist => {
+    this.makePostCallPlaylist(playlist).then(callResult => {
+      if (callResult.status === 201) {
+        this.setState({ playlists: [...this.state.playlists, callResult.data] })
+      }
+    })
+  }
 
-
-   makePostCallPlaylist(playlist){
-      return axios.post('http://localhost:5000/inventories', playlist)
-       .then(function (response) {
-         console.log(response);
-         return response;
-       })
-       .catch(function (error) {
-         console.log(error);
-         return false;
-       });
-   }
-
-
-   selectPlaylist = index => {
-      const { playlists } = this.state
-      
-      axios.get("http://localhost:5000/inventories/".concat(playlists[index]._id)).then(res => {
-	  const song_ids = res.data.songs;
-	  var songs = {songs: []};
-	  var len = Object.keys(song_ids).length
-	  for (var i=0; i < len; i++) {
-		  axios.get("http://localhost:5000/songs/".concat(String(song_ids[i]))).then(res => {
-			  songs.songs.push(res.data);
-		  })
-	  }
-	  this.setState(songs);
-      })
-   }
-
-
-   render() {
-      const { songs } = this.state
-      const { playlists } = this.state
-
-      return (
-        <div className="container">
-          <Table songData={songs}
-	      playlistData={playlists}
-	      removeSong={this.removeSong}
-	      removePlaylist={this.removePlaylist}
-	      selectPlaylist={this.selectPlaylist}
-	      addSong={this.addSong} />
-
-          <Form handleSubmit={this.handleSubmit} />
-        </div>
-      )
-   }
-
-   componentDidMount() {
-      axios.get('http://localhost:5000/songs').then(res => {
-          const songs = res.data.songs;
-          this.setState({ songs });
+  makePostCallPlaylist (playlist) {
+    return axios.post('http://localhost:5000/inventories', playlist)
+      .then(function (response) {
+        console.log(response)
+        return response
       })
       .catch(function (error) {
-          //Not handling the error. Just logging into the console.
-          console.log(error);
-      });
-      axios.get('http://localhost:5000/inventories').then(res => {
-	  const playlists = res.data.playlists;
-	  this.setState({ playlists });
+        console.log(error)
+        return false
       })
+  }
+
+  selectPlaylist = index => {
+    const { playlists } = this.state
+
+    console.log('Running select')
+
+    this.setState({
+      songs: []
+    })
+
+    //Need to preserve this so we can access the state from inside the map statement
+    let thisComponent = this
+
+    axios.get('http://localhost:5000/inventories/'.concat(playlists[index]._id)).then(res => {
+      const song_ids = res.data.songs
+
+      song_ids.map((id) => {
+        return axios.get('http://localhost:5000/songs/'.concat(String(id))).then(function (res) {
+          thisComponent.setState({
+            songs: [...thisComponent.state.songs, res.data]
+          })
+        })
+      })
+    })
+  }
+
+  render () {
+    const { songs } = this.state
+    const { playlists } = this.state
+
+    console.log(JSON.stringify(songs))
+
+    return (
+      <div className="container">
+        <Table songData={songs}
+               playlistData={playlists}
+               removeSong={this.removeSong}
+               removePlaylist={this.removePlaylist}
+               selectPlaylist={this.selectPlaylist}
+               addSong={this.addSong}/>
+
+        <Form handleSubmit={this.handleSubmit}/>
+      </div>
+    )
+  }
+
+  componentDidMount () {
+    axios.get('http://localhost:5000/songs').then(res => {
+      const songs = res.data.songs
+      this.setState({ songs })
+    })
       .catch(function (error) {
-	  console.log(error);
-      });
+        //Not handling the error. Just logging into the console.
+        console.log(error)
+      })
+    axios.get('http://localhost:5000/inventories').then(res => {
+      const playlists = res.data.playlists
+      this.setState({ playlists })
+    })
+      .catch(function (error) {
+        console.log(error)
+      })
   }
 }
 
--- frontend/src/Table.js
+++ frontend/src/Table.js
@@ -2,93 +2,95 @@ import React, { Component } from 'react'
 
 // Table Header for songs in selected playlist
 const TableHeader = () => {
-   return (
-     <thead>
-       <tr>
-	 <th>Song ID</th>
-         <th>Artist</th>
-         <th>Title</th>
-	 <th>Genre</th>
-	 <th>Duration</th>
-	 <th>Cover Art</th>
-       </tr>
-     </thead>
-     )
+  return (
+    <thead>
+    <tr>
+      <th>Song ID</th>
+      <th>Artist</th>
+      <th>Title</th>
+      <th>Genre</th>
+      <th>Duration</th>
+      <th>Cover Art</th>
+    </tr>
+    </thead>
+  )
 }
 
 // Table Header for playlists
 const TableHeader2 = () => {
-   return (
-     <thead>
-       <tr>
-	 <th>Playlist Name</th>
-       </tr>
-     </thead>
-   )
+  return (
+    <thead>
+    <tr>
+      <th>Playlist Name</th>
+    </tr>
+    </thead>
+  )
 }
 
 // Table Body for songs in selected playlist
 const TableBody = props => {
-   console.log(props.songData)
-   const rows = props.songData.map((row, index) => {
-     console.log(row)
-     console.log(index)
-      return (
-        <tr key={index}>
-	  <td>{row._id}</td>
-          <td>{row.artist}</td>
-          <td>{row.title}</td>
-	  <td>{row.genre}</td>
-	  <td>{row.durMin}:{row.durSec}</td>
-	  <td>{row.image}</td>
-	  <td>
-	    <button onClick={() => props.addSong(index)}>Add to Playlist</button>
-	  </td>
-          <td>
-            <button onClick={() => props.removeSong(index)}>Delete</button>
-          </td>
-        </tr>
-      )
-   })
-   console.log(rows)
+  //console.log("songData")
+  //console.log(JSON.stringify(props.songData))
+  let i = 0
 
-   return <tbody>{rows}</tbody>
+  let rows = props.songData.map((row, index) => {
+    //console.log("map")
+    i++
+    return (
+      <tr key={index}>
+        <td>{row._id}</td>
+        <td>{row.artist}</td>
+        <td>{row.title}</td>
+        <td>{row.genre}</td>
+        <td>{row.durMin}:{row.durSec}</td>
+        <td>{row.image}</td>
+        <td>
+          <button onClick={() => props.addSong(index)}>Add to Playlist</button>
+        </td>
+        <td>
+          <button onClick={() => props.removeSong(index)}>Delete</button>
+        </td>
+      </tr>
+    )
+  })
+  //console.log("map ran " + i + "times")
+  //console.log(rows)
+
+  return <tbody>{rows}</tbody>
 }
 
 // Table Body for playlists
 const TableBody2 = props => {
-   const rows = props.playlistData.map((row, index) => {
-     console.log(row)
-     console.log(index)
-      return (
-	<tr key={index}>
-	  <td>{row.title}</td>
-	  <td>
-	    <button onClick={() => props.removePlaylist(index)}>Delete</button>
-	  </td>
-	  <td>
-            <button onClick={() => props.selectPlaylist(index)}>Select</button>
-          </td>
-	</tr>
-      )
-   })
 
-   return <tbody>{rows}</tbody>
+  const rows = props.playlistData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.title}</td>
+        <td>
+          <button onClick={() => props.removePlaylist(index)}>Delete</button>
+        </td>
+        <td>
+          <button onClick={() => props.selectPlaylist(index)}>Select</button>
+        </td>
+      </tr>
+    )
+  })
+
+  return <tbody>{rows}</tbody>
 }
-	      
 
 const Table = props => {
-   const { songData, playlistData, removeSong, removePlaylist, selectPlaylist, addSong } = props
-   
-   return (
-     <table>
-       <TableHeader />
-       <TableBody songData={songData} removeSong={removeSong} />
-       <TableHeader2 />
-       <TableBody2 playlistData={playlistData} removePlaylist={removePlaylist} selectPlaylist={selectPlaylist} />
-     </table>
+  const { songData, playlistData, removeSong, removePlaylist, selectPlaylist, addSong } = props
 
-   )
+  return (
+    <table>
+      <TableHeader/>
+      <TableBody songData={songData} removeSong={removeSong}/>
+      <TableHeader2/>
+      <TableBody2 playlistData={playlistData} removePlaylist={removePlaylist} selectPlaylist={selectPlaylist}/>
+    </table>
+
+  )
 }
 
 export default Table
